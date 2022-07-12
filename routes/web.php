b2e321310d4156a1c7954888d854855be76ce8b6
sanchit237 <sanchitnemlekar8@gmail.com>
2022-07-12 22:51:56 +0530

    Routing

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return view('welcome');
+    return view('welcome');
+});
+
+Route::get('/demo', function () {
     return view('Demo');
 });
+
+//Route with a only view
+Route::view('/about','about');
