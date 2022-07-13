f5ab7a579b3ae9f42258b56a63cba7fe4bb68b01
sanchit237 <sanchitnemlekar8@gmail.com>
2022-07-13 22:45:03 +0530

    Created new component

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
+    <h1>Header File </h1>
+</div>
--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,1 +1,2 @@
+<x-header content="text"/>
 <h1>First Demo View {{$user}}</h1>
