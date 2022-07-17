a693b54bb7b08b9f79e70f772fec1da71b5ae502
sanchit237 <sanchitnemlekar8@gmail.com>
2022-07-17 14:57:30 +0530

    Form Validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class FormController extends Controller
 {
     function form(Request $req){
+
+        $req->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
         return $req->input();
     }
 }
--- resources/views/formview.blade.php
+++ resources/views/formview.blade.php
@@ -1,10 +1,22 @@
 <h1>USER LOGIN</h1>
-
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
 <form action="formdata" method="POST">
     @csrf
+    <label for="">Enter Username</label><br>
     <input type="text" name ="username">
-    <br><br>
+    <p>@error('username'){{$message}}@enderror</p>
+
+    <label for="">Enter Password</label><br>
     <input type="password" name ="password">
-    <br><br>
+    <p>@error('password'){{$message}}@enderror</p>
+
     <button type="submit">Login</button>
 </form>
