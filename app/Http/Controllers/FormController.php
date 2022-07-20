93bc46e7e025dc1727c9e58c1549dcfbe6531cc8
sanchit237 <sanchitnemlekar8@gmail.com>
2022-07-20 22:34:54 +0530

    Saving or inserting data using eloquent model

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,18 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\customer;
 
 class FormController extends Controller
 {
     function form(Request $req){
 
+        //form validation
         $req->validate([
             'username' => 'required',
             'password' => 'required',
         ]);
-        $data = $req->input('username');
-        $path = $req->file('upload')->store('img');
-        $req->session()->put('user', $data);
+        // $data = $req->input('username'); //capture input
+        // $path = $req->file('upload')->store('img'); // file upload
+        // $req->session()->put('user', $data); //add session
+        // return redirect('about'); // redirect
+
+        //save data using eloquent model
+        $custom = new customer;
+        $custom->FirstName = $req->username;
+        $custom->LastName = $req->password;
+        $custom->save();
         return redirect('about');
     }
 }
